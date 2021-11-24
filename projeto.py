76d3c2f2198416d5e19e29164bbcde1a1d4b6915
Fernando-crz <fernando.cruz.ctt@gmail.com>
2021-11-24 19:00:38 -0300

    adcionada funcao eh_ramo_fechado()

--- projeto.py
+++ projeto.py
@@ -1,10 +1,13 @@
 from code_base_EP1 import *
 
 class Marcada():
-	def __init__(self, valor: bool, formula: Proposition): 
+	def __init__(self, valor: bool, formula): 
 		self.valor = valor
-		self.formula = formula
-		self.children = formula.children
+		if isinstance(formula, str):
+			self.formula = converter_formula(formula)
+		else:
+			self.formula = formula
+		self.children = self.formula.children
 
 	def __str__(self):
 		return f"[T] {self.formula}" if self.valor else f"[F] {self.formula}"
@@ -12,10 +15,13 @@ class Marcada():
 	def __repr__(self):
 		return f"[T] {self.formula}" if self.valor else f"[F] {self.formula}"
 
-	def eh_saturada(self):
+	def obtem_tamanho(self) -> int:
+		return len(self.children) + 1
+
+	def eh_saturada(self) -> bool:
 		return len(self.children) == 0
 
-	def escolhe_expansao(self):
+	def obtem_expansao(self) -> bool:
 		"""
 		Avalia se expansao em objeto marcado eh alfa ou beta, retornando true para alfa e false para beta. 
 		"""
@@ -53,27 +59,59 @@ def converter_tableaux(formula: str) -> list:
 	for f in temp:
 		formulas.append(f)
 	formulas = [converter_formula(f) for f in formulas]
-	
 	for i,f in enumerate(formulas):
 		if (i == len(formulas) - 1):
 			marcadas.append(Marcada(False, f))
 			continue
 		marcadas.append(Marcada(True, f))
 
+
 	return marcadas
 
+def eh_ramo_fechado(ramo: list) -> bool:
+	"""
+	Verifica em ramo(lista de marcas) se o mesmo esta fechado.
+	"""
+	expressoes_saturadas = []
+	for marcada in ramo:
+		if marcada.eh_saturada():
+			for saturada in expressoes_saturadas:
+				if saturada.formula == marcada.formula and saturada.valor != marcada.valor:
+					return True
+			expressoes_saturadas.append(marcada)
+	return False
+			
+
+
+def provar_validade(ramo: list) -> bool:
+	pass # a implementar
 
 def main():
 	formula1 = converter_formula("(P >> ~(Q >> R) & (Q >> (P|~R)))")
 	
 	marcada = Marcada(False, formula1)
-	if marcada.escolhe_expansao():
+	if marcada.obtem_expansao():
 		print("ALFA!")
 	else:
 		print("BETA!")
+	print(marcada.children)
 
 	marcadas = converter_tableaux("P >> Q, Q >> R # P >> R")
+	valores = []
+	tamanho_final = 0
+	for m in marcadas:
+		valores.append("ALFA") if m.obtem_expansao() else valores.append("BETA")
+		tamanho_final += m.obtem_tamanho()
+	print(f"tamanho final: {tamanho_final}")
+	print(valores)
 	print(marcadas)
+
+	ramo_exemplo = [Marcada(True, "P >> Q"), Marcada(True, "Q >> R"), Marcada(False, "P >> R"), Marcada(True, "P"), Marcada(False, "R"), Marcada(False, "Q")]
+	print(ramo_exemplo)
+	print(f"Eh ramo fechado?: {eh_ramo_fechado(ramo_exemplo)}")
+	ramo_exemplo.append(Marcada(False, "P"))
+	print(ramo_exemplo)
+	print(f"Eh ramo fechado?: {eh_ramo_fechado(ramo_exemplo)}")
 
 
 if __name__=="__main__":
