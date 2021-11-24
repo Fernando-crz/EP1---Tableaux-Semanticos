5b0a41a6edd3417ee013b695bb88b2cdec247a19
Fernando-crz <fernando.cruz.ctt@gmail.com>
2021-11-24 16:44:59 -0300

    adicionada funcao escolhe_expansao para Marcada()

--- projeto.py
+++ projeto.py
@@ -9,6 +9,21 @@ class Marcada():
 	def __str__(self):
 		return f"[T] {self.formula}" if self.valor else f"[F] {self.formula}"
 
+	def eh_saturada(self):
+		return len(self.children) == 0
+
+	def escolhe_expansao(self):
+		"""
+		Avalia se expansao em objeto marcado eh alfa ou beta, retornando true para alfa e false para beta. 
+		"""
+		if self.valor:
+			if isinstance(self.formula, And) or isinstance(self.formula, Not): return True
+			if isinstance(self.formula, Or) or isinstance(self.formula, Implies): return False
+		else:
+			if isinstance(self.formula, And) or isinstance(self.formula, Not): return False
+			if isinstance(self.formula, Or) or isinstance(self.formula, Implies): return True
+
+
 def para_variavel(variavel: str) -> Variable:
 	return Variable(variavel)
 
@@ -21,8 +36,17 @@ def converter_formula(formula: str) -> Proposition:
 	return eval(formula)	# retornamos a expressao convertida usando eval()
 
 def main():
-	formula1 = converter_formula("(P >> (Q >> R)| (Q >> (P|~R)))")
-	print(formula1)
+	formula1 = converter_formula("(P >> ~(Q >> R) & (Q >> (P|~R)))")
+	marcada = Marcada(False, formula1)
+	if marcada.escolhe_expansao():
+		print("ALFA!")
+	else:
+		print("BETA!")
+	# nodes = get_nodes(formula1)
+	# for node in nodes:
+	# 	print(f"NODE: {node} , tamanho: {len(node.children)}")
+	# 	print(f"TIPO: {type(node)}")
+	# 	if isinstance(node, Not): print("EH NOT!!!!11!!")
 	
 
 if __name__=="__main__":
