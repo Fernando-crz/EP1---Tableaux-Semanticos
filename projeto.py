38622de423a53832ca3fbd62076d04abae092b61
Fernando-crz <fernando.cruz.ctt@gmail.com>
2021-11-24 17:06:38 -0300

    adicionada funcao converter_tableaux()

--- projeto.py
+++ projeto.py
@@ -28,6 +28,9 @@ def para_variavel(variavel: str) -> Variable:
 	return Variable(variavel)
 
 def converter_formula(formula: str) -> Proposition:
+	"""
+	Recebe de input uma formula em formato de string e a retorna em um objeto Proposition
+	"""
 	termos = " ><|&~()"
 	dic_variaveis = formula.maketrans(termos," "*len(termos))	# retira 'termos' de formula e guarda convesoes em dicionario 
 	variaveis = set(formula.translate(dic_variaveis).split())	# 'traduz' formula usando dicionario anterior, e retorna um set com variaveis
@@ -35,19 +38,40 @@ def converter_formula(formula: str) -> Proposition:
 		exec(f"{variavel} = para_variavel('{variavel}')")	# associamos cada variavel ao seu respectivo nome com exec
 	return eval(formula)	# retornamos a expressao convertida usando eval()
 
+def converter_tableaux(formula: str) -> list:
+	"""
+	Recebe de input um tableaux em formato de sting e retorna uma lista de formulas marcadas
+	"""
+	marcadas = []
+
+	formulas = formula.split(",")
+	temp = formulas.pop().split("#")
+	
+	for f in temp:
+		formulas.append(f)
+	formulas = [converter_formula(f) for f in formulas]
+	
+	for i,f in enumerate(formulas):
+		if (i == len(formulas) - 1):
+			marcadas.append(Marcada(False, f))
+			continue
+		marcadas.append(Marcada(True, f))
+
+	return marcadas
+
+
 def main():
 	formula1 = converter_formula("(P >> ~(Q >> R) & (Q >> (P|~R)))")
+	
 	marcada = Marcada(False, formula1)
 	if marcada.escolhe_expansao():
 		print("ALFA!")
 	else:
 		print("BETA!")
-	# nodes = get_nodes(formula1)
-	# for node in nodes:
-	# 	print(f"NODE: {node} , tamanho: {len(node.children)}")
-	# 	print(f"TIPO: {type(node)}")
-	# 	if isinstance(node, Not): print("EH NOT!!!!11!!")
-	
+
+	marcadas = converter_tableaux("P >> Q, Q >> R # P >> R")
+	print(marcadas)
+
 
 if __name__=="__main__":
 	main()
