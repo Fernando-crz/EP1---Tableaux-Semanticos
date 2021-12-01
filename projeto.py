cf426993a391c1f40c4444a9fc88d2075d6dfe0f
Fernando-crz <fernando.cruz.ctt@gmail.com>
2021-12-01 18:35:50 -0300

    versão 1.0

--- projeto.py
+++ projeto.py
@@ -1,5 +1,12 @@
 from code_base_EP1 import *
 
+def cria_lista_unicos(lista):
+	resultado = []
+	for elemento in lista:
+		if not elemento in resultado:
+			resultado.append(elemento)
+	return resultado
+
 class Marcada():
 	def __init__(self, valor: bool, formula): 
 		self.valor = valor
@@ -21,6 +28,17 @@ class Marcada():
 	def __eq__(self, marcada):
 		return self.valor == marcada.valor and self.formula == marcada.formula
 
+<<<<<<< Updated upstream
+=======
+	@staticmethod
+	def converter_formula(formula: str) -> Proposition:
+		termos = " ><|&~()"
+		dic_variaveis = formula.maketrans(termos," "*len(termos))	# retira 'termos' de formula e guarda convesoes em dicionario 
+		variaveis = set(formula.translate(dic_variaveis).split())	# 'traduz' formula usando dicionario anterior, e retorna um set com variaveis
+		for variavel in variaveis:
+			exec(f"{variavel} = para_variavel('{variavel}')")	# associamos cada variavel ao seu respectivo nome com exec
+		return eval(formula)	# retornamos a expressao convertida usando eval()
+>>>>>>> Stashed changes
 
 	def obtem_tamanho(self) -> int:
 		return len(self.children) + 1
@@ -41,6 +59,7 @@ class Marcada():
 			if isinstance(self.formula, And) or isinstance(self.formula, Not): return 1
 			if isinstance(self.formula, Or) or isinstance(self.formula, Implies): return 0
 
+<<<<<<< Updated upstream
 
 def para_variavel(variavel: str) -> Variable:
 	return Variable(variavel)
@@ -61,21 +80,137 @@ def converter_tableaux(formula: str) -> list:
 	Recebe de input um tableaux em formato de sting e retorna uma lista de formulas marcadas
 	"""
 	marcadas = []
+=======
+class Ramo():
+	def __init__(self, marcadas, marcas = None):
+		if isinstance(marcadas, str):
+			self.marcadas = self.converter_tableaux(marcadas)
+		else:
+			self.marcadas = marcadas
+		self.marcas = marcas
+		if self.marcas == None:
+			self.marcas = []
+			for m in self.marcadas:
+				self.marcas.append(m.obtem_expansao())
+		self.tamanho = sum([marcada.obtem_tamanho() for marcada in self.marcadas])
 
-	formulas = formula.split(",")
-	temp = formulas.pop().split("#")
+	def __add__(self, marcada):
+		return Ramo(self.marcadas+[marcada], marcas = self.marcas + [marcada.obtem_expansao()])
+
+	def __str__(self):
+		return f"{self.marcadas}"
+
+	@staticmethod
+	def aplicar_alfas(ramo):
+		for i,marcada in enumerate(ramo.marcadas): 
+			if ramo.marcas[i] == 0:
+				# print(len(ramo.marcas))
+				if marcada.valor:
+					if isinstance(marcada.formula, And):
+						for elemento in marcada.children:
+							ramo = ramo + Marcada(True, elemento)
+					if isinstance(marcada.formula, Not):
+						ramo = ramo + Marcada(False, marcada.children[0])
+				else:
+					if isinstance(marcada.formula, Or):
+						for elemento in marcada.children:
+							ramo = ramo + Marcada(False, elemento)
+					if isinstance(marcada.formula, Implies):
+						ramo = ramo + Marcada(True, marcada.children[0])
+						ramo = ramo + Marcada(False, marcada.children[1])
+				
+				ramo.marcas[i] = -1
+		return ramo
 	
-	for f in temp:
-		formulas.append(f)
-	formulas = [converter_formula(f) for f in formulas]
-	for i,f in enumerate(formulas):
-		if (i == len(formulas) - 1):
-			marcadas.append(Marcada(False, f))
-			continue
-		marcadas.append(Marcada(True, f))
+	def obter_saturacoes(self):
+		resultado = []
+		for marcada in self.marcadas:
+			if marcada.eh_saturada():
+				resultado.append(marcada)
+		return cria_lista_unicos(resultado)
+	
+	def verificar_se_fechado(self):
+		atomos = self.obter_saturacoes()
+		for atomo in atomos:
+			if ~atomo in atomos:
+				return True
+		return False
+	
+	@staticmethod
+	def converter_tableaux(formula: str) -> list:
+		"""
+		Recebe de input um tableaux em formato de sting e retorna uma lista de formulas marcadas
+		"""
+		marcadas = []
+>>>>>>> Stashed changes
+
+		formulas = formula.split(",")
+		temp = formulas.pop().split("#")
+		
+		for f in temp:
+			formulas.append(f)
+		formulas = [Marcada.converter_formula(f) for f in formulas]
+		for i,f in enumerate(formulas):
+			if (i == len(formulas) - 1):
+				marcadas.append(Marcada(False, f))
+				continue
+			marcadas.append(Marcada(True, f))
 
 
+<<<<<<< Updated upstream
 	return marcadas
+=======
+		return marcadas
+
+	@staticmethod
+	def extrair_beta(ramo):
+		try:
+			local = ramo.marcas[::-1].index(1)
+		except ValueError:
+			return False
+
+		local = len(ramo.marcas) - local - 1
+
+		marcada = ramo.marcadas[local]
+		retorno = []
+		if marcada.valor:
+			if isinstance(marcada.formula, Or):
+				for elemento in marcada.children:
+					retorno.append(Marcada(True, elemento))
+			if isinstance(marcada.formula, Implies):
+				retorno.append(Marcada(False, marcada.children[0]))
+				retorno.append(Marcada(True, marcada.children[1]))
+		else:
+			if isinstance(marcada.formula, And):
+				for elemento in marcada.children:
+					retorno.append(Marcada(False, elemento))
+			if isinstance(marcada.formula, Not):
+				retorno.append(Marcada(True, marcada.children[0]))
+		ramo.marcas[local] = -1
+		return (retorno, ramo)
+			
+
+	@staticmethod
+	def provar_validade(ramo):
+		ramo = Ramo.aplicar_alfas(ramo)
+		# print(ramo.marcadas)
+		if ramo.verificar_se_fechado():
+			return True
+		# print(ramo.marcas)
+		operacao = Ramo.extrair_beta(ramo)
+		# print(operacao)
+		if operacao == False:
+			return ramo.obter_saturacoes()
+		betas, ramo = operacao
+		for beta in betas:
+			retorno = Ramo.provar_validade(ramo + beta)
+			if isinstance(retorno, list): return retorno
+
+		return True
+
+def para_variavel(variavel: str) -> Variable:
+	return Variable(variavel)
+>>>>>>> Stashed changes
 
 def eh_ramo_fechado(ramo: list) -> bool:
 	"""
@@ -89,38 +224,6 @@ def eh_ramo_fechado(ramo: list) -> bool:
 					return True
 			expressoes_saturadas.append(marcada)
 	return False
-			
-def aplicar_alfa_ramo(ramo: list, lo: int, hi: int):
-	"""
-	Expande todas expressoes alfa em ramo, adicionando elementos encontrados ao ramo
-	"""
-	ramo_utilizado = ramo[lo:hi]
-
-	for marcada in ramo_utilizado:
-
-		if marcada.obtem_expansao() == 0:
-			if marcada.valor:
-				if isinstance(marcada.formula, And):
-					for children in marcada.children:
-						ramo.append(Marcada(True,children))
-				if isinstance(marcada.formula, Not):
-					ramo.append(Marcada(False,marcada.children[0]))
-			else:
-				if isinstance(marcada.formula, Or):
-					for children in marcada.children:
-						ramo.append(Marcada(False,children))
-				if isinstance(marcada.formula, Implies):
-					ramo.append(Marcada(True,marcada.children[0]))
-					ramo.append(Marcada(False,marcada.children[1]))
-
-def aplicar_alfa_iter(ramo: list):
-	n = 0
-	new_n = len(ramo)
-	while (new_n != n):
-		aplicar_alfa_ramo(ramo, n, new_n)
-		n = len(ramo)
-		aplicar_alfa_ramo(ramo, new_n, n)
-		new_n = len(ramo)
 
 def aplicar_beta(marcada: Marcada) -> tuple:
 	"""
@@ -138,57 +241,40 @@ def aplicar_beta(marcada: Marcada) -> tuple:
 			return (Marcada(True,marcada.children[0]))
 	return -1
 
-def cria_marcacao_beta(ramo: list) -> list:
-	betas = []
-	for marcada in ramo:
-		if marcada.eh_saturada():
-			betas.append(False)
-			continue
-		betas.append(not marcada.obtem_expansao())
-	return betas
-
-def provar_validade(ramo: list) -> bool:
-	pilha = []
-	while True:
-		aplicar_alfa_ramo(ramo)
-		betas = cria_marcacao_beta(ramo)
-		if eh_ramo_fechado(ramo):
-			print("Valido!")
-			return True
-	return False
-
-
 def main():
-	formula1 = converter_formula("(P >> ~(Q >> R) & (Q >> (P|~R)))")
+	# formula1 = converter_formula("(P >> ~(Q >> R) & (Q >> (P|~R)))")
 	
-	marcada = Marcada(False, formula1)
-	if marcada.obtem_expansao():
-		print("ALFA!")
-	else:
-		print("BETA!")
-	print(marcada.children)
+	# marcada = Marcada(False, formula1)
+	# if marcada.obtem_expansao():
+	# 	print("ALFA!")
+	# else:
+	# 	print("BETA!")
+	# print(marcada.children)
 
-	marcadas = converter_tableaux("P >> Q, Q >> R # P >> R")
-	valores = []
-	tamanho_final = 0
-	for m in marcadas:
-		valores.append("ALFA") if m.obtem_expansao() else valores.append("BETA")
-		tamanho_final += m.obtem_tamanho()
-	print(f"tamanho final: {tamanho_final}")
-	print(valores)
-	print(marcadas)
+	# marcadas = converter_tableaux("P >> Q, Q >> R # P >> R")
+	# valores = []
+	# tamanho_final = 0
+	# for m in marcadas:
+	# 	valores.append("ALFA") if m.obtem_expansao() else valores.append("BETA")
+	# 	tamanho_final += m.obtem_tamanho()
+	# print(f"tamanho final: {tamanho_final}")
+	# print(valores)
+	# print(marcadas)
 
-	ramo_exemplo = [Marcada(True, "P >> Q"), Marcada(True, "Q >> R"), Marcada(False, "P >> R"), Marcada(True, "P"), Marcada(False, "R"), Marcada(False, "Q")]
-	print(ramo_exemplo)
-	print(f"Eh ramo fechado?: {eh_ramo_fechado(ramo_exemplo)}")
-	ramo_exemplo.append(Marcada(False, "P"))
-	print(ramo_exemplo)
-	print(f"Eh ramo fechado?: {eh_ramo_fechado(ramo_exemplo)}")
+	# ramo_exemplo = [Marcada(True, "P >> Q"), Marcada(True, "Q >> R"), Marcada(False, "P >> R"), Marcada(True, "P"), Marcada(False, "R"), Marcada(False, "Q")]
+	# print(ramo_exemplo)
+	# print(f"Eh ramo fechado?: {eh_ramo_fechado(ramo_exemplo)}")
+	# ramo_exemplo.append(Marcada(False, "P"))
+	# print(ramo_exemplo)
+	# print(f"Eh ramo fechado?: {eh_ramo_fechado(ramo_exemplo)}")
 	
-	ramo_ex_2 = [Marcada(True,"A & (B&C)"), Marcada(False, "(D & E) >> B")]
+	# ramo_ex_2 = [Marcada(True,"A & (B&C)"), Marcada(False, "(D & E) >> B")]
 
-	aplicar_alfa_iter(ramo_ex_2)
-	print(eh_ramo_fechado(ramo_ex_2))
+	# aplicar_alfa_iter(ramo_ex_2)
+	# print(eh_ramo_fechado(ramo_ex_2))
+ 
+	formula = Ramo("~(P | Q) >> ~P & ~Q")
+	print(Ramo.provar_validade(formula))
 
 
 if __name__=="__main__":
